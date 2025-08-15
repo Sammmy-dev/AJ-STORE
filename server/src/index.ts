396b7f2a71afda5e4ef89fb3e103360f145d12be
Samuel <ajiboyeadebayo901@gmail.com>
2025-08-15 13:49:48 +0100

    dashboard controller and route setup

---
+++ server/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv"
+import bodyParser from "body-parser"
+import cors from "cors"
+import helmet from "helmet";
+import morgan from "morgan";
+import { Server } from "http";
+
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+
+// Routes
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Server
+const port = process.env.PORT || 3001;
+const server = new Server(app);
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
